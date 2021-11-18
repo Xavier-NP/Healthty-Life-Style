efc8719d9767d13f21e1b62d9c9cb61b5d63a7d8
Xavier-NP <s10208449@connect.np.edu.sg>
2021-11-18 17:52:26 +0800

    Added Diary class to api.py, incomplete API

--- WebApp/website/api.py
+++ WebApp/website/api.py
@@ -1,20 +1,35 @@
+from datetime import datetime
 from . import db
-from .models import Notes
+from .models import Note
 from ..main import api 
-from flask_restful import Api,Resource,reqparse
+from flask_restful import Api,Resource,reqparse,fields,marshal_with
 from flask import request
-test ={}
 
 #Mobile Diary
-dairy_post_args = reqparse.RequestParser()
-dairy_post_args.add_argument("")
+diary_post_args = reqparse.RequestParser()
+diary_post_args.add_argument("data",type=str,help="Content of the Note!",required=True)
+diary_post_args.add_argument("user_id",type=int,help="User id",required=True)
+
+resource_fields = {
+    'id':fields.Integer,
+    'user_id':fields.Integer,
+    'data':fields.String
+}
 class Diary(Resource):
-    def get(self,diary_id):
-        return test[diary_id]
-    def post(self,diary_id):
-        return   
+    @marshal_with(resource_fields)
+    def get(self,note_id):
+        result = Note.query.get(user_id=note_id)
+        return result
+    
+    @marshal_with(resource_fields)
+    def post(self,note_id):
+        args = diary_post_args.parse_args()
+        note = Note(id=note_id,data=args['data'],user_id=args['user_id'])
+        db.session.add()   
+        db.session.commit()
+        return note,201
          
-api.add_resource(Diary,"/dairy/<diary_id>")
+api.add_resource(Diary,"/dairy/<int:note_id>")
 
 
             
